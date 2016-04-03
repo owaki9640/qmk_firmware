1b43d7cf6799015be39f4dcaaed1f82199f85d23
Takeshi Owaki <owaki@kumanomi.org>
2016-04-03 18:14:25 +0900

    Add ~L1 to BASE layer

--- keyboard/ergodox_ez/keymaps/owaki/keymap.c
+++ keyboard/ergodox_ez/keymaps/owaki/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  L3  |  ~L2 |  NO  |   [  |   ]  |                                       | Left | Down |  Up  | Right|  L3  |
+ *   |  L3  |  ~L2 |  ~L1 |   [  |   ]  |                                       | Left | Down |  Up  | Right|  L3  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |LANG2/| LCtl |       | RCtl |LANG1/|
@@ -35,14 +35,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_GRV,     KC_1,     KC_2,    KC_3,    KC_4,    KC_5,                KC_ESC,
-        KC_TAB,     KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,                TG(KPAD),
-        KC_LCTRL,   KC_A,     KC_S,    KC_D,    KC_F,    KC_G,
-        KC_LSFT,    KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,                ALL_T(KC_NO),
-        TG(BASE_R), MO(MOUS), KC_NO,   KC_LBRC, KC_RBRC,
-                                                         LT(BASE_R,KC_LANG2), KC_LCTL,
-                                                                              KC_LALT,
-                                                KC_ENT,  KC_DEL,              KC_LGUI,
+        KC_GRV,     KC_1,     KC_2,     KC_3,    KC_4,    KC_5,                KC_ESC,
+        KC_TAB,     KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,                TG(KPAD),
+        KC_LCTRL,   KC_A,     KC_S,     KC_D,    KC_F,    KC_G,
+        KC_LSFT,    KC_Z,     KC_X,     KC_C,    KC_V,    KC_B,                ALL_T(KC_NO),
+        TG(BASE_R), MO(MOUS), MO(KPAD), KC_LBRC, KC_RBRC,
+                                                          LT(BASE_R,KC_LANG2), KC_LCTL,
+                                                                               KC_LALT,
+                                                 KC_ENT,  KC_DEL,              KC_LGUI,
         // right hand
              KC_EQL,       KC_6,                KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
              TG(KPAD),     KC_Y,                KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
