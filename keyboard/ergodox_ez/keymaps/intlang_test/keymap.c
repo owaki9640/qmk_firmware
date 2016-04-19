4b3bd7efff84cf977f8f6bf981b3377264da6d5a
Takeshi Owaki <owaki@kumanomi.org>
2016-04-19 11:53:38 +0900

    Add INT4 and INT5

--- keyboard/ergodox_ez/keymaps/intlang_test/keymap.c
+++ keyboard/ergodox_ez/keymaps/intlang_test/keymap.c
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab    |   Q  |   W  |   E  |   R  |   T  | INT1 |           | INT3 |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
- * |--------+------+------+------+------+------| LANG2|           | LANG1|------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------| INT4 |           | INT5 |------+------+------+------+------+--------|
  * | LShift |  Z   |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |  NO  |  Meh | Hyper|   [  |   ]  |                                       | Left | Down |  Up  | Right|  NO  |
@@ -33,21 +33,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,   KC_1,         KC_2,         KC_3,     KC_4,     KC_5,     KC_ESC,
         KC_TAB,   KC_Q,         KC_W,         KC_E,     KC_R,     KC_T,     KC_INT1,
         KC_LCTL,  KC_A,         KC_S,         KC_D,     KC_F,     KC_G,
-        KC_LSFT,  KC_Z,         KC_X,         KC_C,     KC_V,     KC_B,     KC_LANG2,
+        KC_LSFT,  KC_Z,         KC_X,         KC_C,     KC_V,     KC_B,     KC_INT4,
         KC_NO,    MEH_T(KC_NO), ALL_T(KC_NO), KC_LBRC,  KC_RBRC,
                                                                   KC_LANG2, KC_LCTL,
                                                                             KC_LALT,
                                                         KC_ENT,   KC_DEL,   KC_LGUI,
         // right hand
-                KC_EQL,   KC_6,     KC_7,     KC_8,     KC_9,   KC_0,     KC_MINS,
-                KC_INT3,  KC_Y,     KC_U,     KC_I,     KC_O,   KC_P,     KC_BSLS,
-                          KC_H,     KC_J,     KC_K,     KC_L,   KC_SCLN,  KC_QUOT,
-                KC_LANG1, KC_N,     KC_M,     KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+                KC_EQL,  KC_6,      KC_7,     KC_8,     KC_9,   KC_0,     KC_MINS,
+                KC_INT3, KC_Y,      KC_U,     KC_I,     KC_O,   KC_P,     KC_BSLS,
+                         KC_H,      KC_J,     KC_K,     KC_L,   KC_SCLN,  KC_QUOT,
+                KC_INT5, KC_N,      KC_M,     KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
                                     KC_LEFT,  KC_DOWN,  KC_UP,  KC_RGHT,  KC_NO,
-
-                KC_RCTL,  KC_LANG1,
+                KC_RCTL, KC_LANG1,
                 KC_RALT,
-                KC_RGUI,  KC_BSPC,  KC_SPC
+                KC_RGUI, KC_BSPC,  KC_SPC
     ),
 };
 
