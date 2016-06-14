0729402d7c93152b72b4c8b37e6e268f9540736f
Takeshi Owaki <owaki@kumanomi.org>
2016-06-14 11:15:10 +0900

    Revert Makefile and change to use makefile.mk

---
+++ keyboard/ergodox_ez/keymaps/intlang_test/makefile.mk
@@ -0,0 +1,10 @@
+BOOTMAGIC_ENABLE = no # Virtual DIP switch configuration(+1000)
+MOUSEKEY_ENABLE  = no # Mouse keys(+4700)
+EXTRAKEY_ENABLE  = no # Audio control and System control(+450)
+CONSOLE_ENABLE   = no # Console for debug(+400)
+COMMAND_ENABLE   = no # Commands for debug and configuration
+CUSTOM_MATRIX    = yes # Custom matrix file for the ErgoDox EZ
+SLEEP_LED_ENABLE = no # Breathing sleep LED during USB suspend
+NKRO_ENABLE      = no	# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+MIDI_ENABLE      = no # MIDI controls
+UNICODE_ENABLE   = no # Unicode
